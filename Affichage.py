cd7b907bc51c95a03b899dc47dfa676ab6ae32d5
LAPTOP-7NRV6DHG\maelb <brenterchmael@orange.fr>
2024-02-12 17:21:28 +0100

    add an event to stop the programm when escape is clicked

--- Affichage.py
+++ Affichage.py
@@ -7,10 +7,14 @@ class Affichage (init.tk.Tk):
         best_dist = 0
         self.title("G2 - Algo fourmis")
         self.draw_lieu(NB_LIEUX, largeur, hauteur)
-        evolution = init.tk.Label(self, text=f"nombre d'itérations: {nb_it}, meilleure distance: {best_dist}")
-        evolution.pack()
+        self.d_evolution(nb_it, best_dist)
+        self.bind("<Escape>", self.close_f)
         self.mainloop()
 
+
+    def d_evolution(self, nb_it, best_dist):
+        evolution = init.tk.Label(self, text=f"nombre d'itérations: {nb_it}, meilleure distance: {best_dist}")
+        evolution.pack()
 
     def draw_lieu(self, NB_LIEUX, largeur, hauteur):
         self.canvas = init.tk.Canvas(self, width=largeur, height=hauteur, bg='white')
@@ -19,6 +23,11 @@ class Affichage (init.tk.Tk):
             x_max = init.random.randint(0, largeur)
             y_max = init.random.randint(0, hauteur)
             self.canvas.create_oval(x_max-30,y_max, x_max, y_max-30)
+
+
+    def close_f(self, event):
+        self.destroy()
+
 
 def main():
     fenetre = Affichage(init.HAUTEUR, init.LARGEUR, 5)
