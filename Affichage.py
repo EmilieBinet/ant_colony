d3c78a2f70496cc9dfd807a3af3f9863294c396e
LAPTOP-7NRV6DHG\maelb <brenterchmael@orange.fr>
2024-02-16 11:42:28 +0100

    create tsp_aco file

--- Affichage.py
+++ Affichage.py
@@ -1,6 +1,7 @@
 from Graph import Graph
 from Route import Route
 import tkinter as tk
+from tkinter import ttk
 
 class Affichage (tk.Tk):
     def __init__(self, hauteur, largeur, NB_LIEUX) -> None:
@@ -11,6 +12,7 @@ class Affichage (tk.Tk):
         best_dist = 0
         self.title("G2 - Algo fourmis")
         self.draw_lieu(liste_lieux, largeur, hauteur)
+        self.draw_route(liste_lieux[0])
         self.d_evolution(nb_it, best_dist)
         self.bind("<space>", self.d_matrix)
         self.bind("<Escape>", self.close_f)
@@ -32,20 +34,43 @@ class Affichage (tk.Tk):
             self.canvas.create_text(x+15, y+15, text=name, font=("Arial", 15))
             self.canvas.create_oval(x, y, x+30, y+30)
 
-    def d_route():
+
+    def draw_route(self, lieu):
+        x = lieu.x
+        y = lieu.y
+        self.canvas.create_line(x+30, y+30, 623, 25, width=2)
         pass
+
+    def draw_matrix(self, NB_LIEUX, hauteur):
+        for i in range(NB_LIEUX):
+            y = i*(hauteur/(NB_LIEUX))
+            for j in range(NB_LIEUX):
+                x = j*(hauteur/(NB_LIEUX))
+                self.win.canvas.create_text(x+200, y+100, text=self.matrix[i][j], tags=self.matrix[i][j])
+                #self.win.canvas.create_rectangle(x+200,y, x+(hauteur/(NB_LIEUX)), y+(hauteur/(NB_LIEUX)))
+        # for row in self.matrix:
+        #     line = " | ".join(map(str, row))
+        #     self.matrix_text.insert(tk.END, line + '\n')
 
 
     def d_matrix(self, event):
-        win = tk.Toplevel(self)
-        win.title("Matrix of cost")
+        largeur = 800
+        hauteur = 600
+        self.matrix = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+        self.win = tk.Toplevel(self)
+        self.win.title("Matrix of cost")
+        self.win.canvas = tk.Canvas(self.win, width=800, height=600, bg="white")
+        self.win.canvas.pack(anchor=tk.CENTER)
+        # self.matrix_text = tk.Text(self.win, wrap=tk.NONE)
+        # self.matrix_text.pack()
+        
+        self.draw_matrix(3, hauteur)
+        
 
 
     def close_f(self, event):
         self.destroy()
-
-
-def main():
-    fenetre = Affichage(600, 800, 5)
-
-main()
---
+++ tsp_aco.py
@@ -0,0 +1,14 @@
+from define import *
+from Affichage import Affichage
+from Lieu import Lieu
+
+
+class TSP_ACO():
+    def __init__(self) -> None:
+        fenetre = Affichage(HAUTEUR, LARGEUR, NB_LIEUX)
+
+
+def main():
+    app = TSP_ACO()
+
+main()
