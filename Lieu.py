7af8ca093ca06bfc7dbdbb75d1681a0a67fb6f76
LAPTOP-7NRV6DHG\maelb <brenterchmael@orange.fr>
2024-02-16 08:55:29 +0100

    resolve type mistake

--- Lieu.py
+++ Lieu.py
@@ -1,4 +1,5 @@
-from tsp_graph_init import LONGUEUR, LARGEUR, numpy as np
+from tsp_graph_init import HAUTEUR, LARGEUR, DIAMETRE, np
+
 class Lieu:
     x = 0
     y = 0
@@ -8,12 +9,13 @@ class Lieu:
         if xinit is None:
             xinit = np.random.randint(DIAMETRE, LARGEUR - DIAMETRE)
         if yinit is None:
-            yiniy = np.random.randint(DIAMETRE, LONGUEUR - DIAMETRE)
+            yinit = np.random.randint(DIAMETRE, HAUTEUR - DIAMETRE)
         if nameinit is None:
             nameinit = "noname"
         self.x = xinit
         self.y = yinit
         self.name = nameinit
+        print(self.x)
 
     def distance(self, lieu2):
         dist = ((lieu2.x2-self.X)**2 + (lieu2.y2-self.y)**2)**(1/2)
