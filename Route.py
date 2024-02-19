7412a80ef7f966b8c74fa4a86ab9bceadb29c9fc
Emilie Binet <eccb2000@hwu.ac.uk>
2024-02-19 14:32:28 +0100

    update matrice
    Co-authored-by: Enora Boscher <enorab35@users.noreply.github.com>

--- Route.py
+++ Route.py
@@ -4,32 +4,48 @@ from Graph import Graph
 class Route:
 
     def __init__(self, graph=Graph()):
+        self.ordre=[]
+        
+    #Fonctions de comparaison des routes:
+    def __eq__(self, __value: object) -> bool:
+        pass
+    
+    def __ne__(self, __value: object) -> bool:
+        pass
+
+    def __lt__(self, __value: object) -> bool:
+        pass
+
+    def __gt__(self, __value: object) -> bool:
+        pass
+
+    def __ge__(self, __value: object) -> bool:
+        pass
+    
+    def __le__(self, __value: object) -> bool:
+        pass
+
+
+
+    #Fonction pour afficher l'objet route
+    def __repr__(self):
+        return f'Route(\'{self.ordre}\')'
+
+
+    def glouton(self, graph):
         self.ordre = ["0"]
-        dist_total = 0
-        voisin = graph.liste_lieux[:]
-        print(f"Liste_lieux : {graph.liste_lieux}")
-        # del voisin[0] # retire 0 des voisins
+        dist_toal = 0
+        voisin = graph.list_lieux[:]
         while len(self.ordre) != NB_LIEUX:
             lieu_from=[lieu for lieu in graph.liste_lieux if lieu.name == self.ordre[-1]][0]
-            #print(f'lieu passé: {lieu_from}')
             voisin.remove(lieu_from)
-            #print(f'voisin: {voisin}')
             closest = graph.plus_proche_voisin(lieu_from, voisin)
             dist_total = graph.calcul_distance_route(dist_total, self.ordre[-1], closest)
             print(f"Total : {dist_total}")
             self.ordre.append(closest.name)
-            #print(f"ordre petit à petit: {self.ordre}")
         self.ordre.append("0")
-        
 
-    def __eq__(self, __value: object) -> bool:
-        pass
-    
-    
-    def __repr__(self):
-        return f'Route(\'{self.ordre}\')'
-
-    def fourmi_chemin(matrice_a, matrice_b):
+    def ant_route(self, matrice_a, matrice_b):
         self.ordre = ["0"]
         while len(self.ordre) != NB_LIEUX:
             lieu_depart = int(self.ordre[-1])
@@ -44,22 +60,6 @@ class Route:
                     phero_left.append(pheromones[i])
             prob = random.choices(dest_left, weights=[0.5*d+0.5*p for d,p in zip(dest_left, phero_left)], k=1)
             chosen_dest = [id for id, val in enumerate(destinations) if val == prob and id not in self.ordre][0]
-
-
-            # best_proba, best_dest = 0, None
-            # for i in range(NB_LIEUX):
-            #     if i == lieu_depart:
-            #         continue
-            #     else:
-            #         proba = 0.5*depart_a[i] + 0.5*depart_b[i]
-            #         if proba > best_proba:
-            #             best_proba, best_dest = proba, i
+            print(f"Prochain noeud : {chosen_dest}")
             self.ordre.append(str(chosen_dest))
         self.ordre.append("0")
-
-# def random_choice(self,lieu, liste_lieux_restant):
-#     """Choisi aléatoirement le noeud en fonction des probabilités des matrices de phéromones et de distance"""
-#     mat_prob_left = 0.5*self.mat_pheromones[] + 0.5*self.mat_prob_dist #Doit ressortir une liste de probabilités en fonction d'un lieu 
-#     choice = random.choices(liste_lieux_restant, weights=mat_prob_left, k=1)
-#     return choice
-
