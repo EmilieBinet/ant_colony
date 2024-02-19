e4b83fd022106b4816afa094e951995bb1c63f85
Enora Boscher <enoraboscher@gmail.com>
2024-02-19 16:56:35 +0100

    patch remove error

--- Route.py
+++ Route.py
@@ -46,7 +46,7 @@ class Route:
         voisin = graph.liste_lieux[:]
         while len(self.ordre) != NB_LIEUX:
             lieu_from=[lieu for lieu in graph.liste_lieux if lieu.name == self.ordre[-1]][0]
-            print(lieu_from)
+            # print(lieu_from)
             voisin.remove(lieu_from)
             # print(f"matrice_cout: {graph.matrice_cout.matrice}")
             destinations = graph.matrice_cout.matrice[int(lieu_from.name)]#cout_od
@@ -64,8 +64,8 @@ class Route:
             prob_dest = [(1/d)/(sum(0 if elem == 0 else 1/elem for elem in dest_left)) for d in dest_left]
             # print(f"prob_dest: {prob_dest}")
 
-            prob = random.choices(dest_left, weights=[0.5*d+0.5*p for d,p in zip(prob_dest, phero_left)], k=1)
-            chosen_dest = [id for id, val in enumerate(destinations) if val == prob and id not in self.ordre][0]
+            prob = random.choices(dest_left, weights=[0.4*d+0.6*p for d,p in zip(prob_dest, phero_left)], k=1)
+            chosen_dest = [id for id, val in enumerate(destinations) if val == prob and str(id) not in self.ordre][0]
             # print(f"Prochain noeud : {chosen_dest}")
             self.dist_total = graph.calcul_distance_route(self.dist_total, int(self.ordre[-1]), chosen_dest)
             # print(f"Total : {self.dist_total}")
